a9b76ebcdff883d3d067a92ee0100f7ec188e74f
Dominic <dom@deadempires.net>
2012-06-29 10:23:24 +0100

    learn.py tool to automatically grab tweets from specific twitter accounts

--- config-example.py
+++ config-example.py
@@ -1,6 +1,10 @@
+# Get these from dev.twitter.com
 api = {
 	'consumer_key': '',
 	'consumer_secret': '',
 	'access_token_key': '',
 	'access_token_secret': ''
 }
+
+# List of accounts that learn.py should grab tweets from
+dump_accounts = []
---
+++ learn.py
@@ -0,0 +1,38 @@
+import twitter, os
+import config
+from simplejson import loads, dumps
+from cobe.brain import Brain
+
+b = Brain(os.path.join(os.path.dirname(__file__), 'cobe.brain'))
+
+try:
+	state = loads(open(os.path.join(os.path.dirname(__file__), '.state'), 'r').read())
+except:
+	state = {'accounts': {}}
+
+api = twitter.Api(**config.api)
+
+b.start_batch_learning()
+tweets = 0
+for account in config.dump_accounts:
+	print "Grabbing tweets for %s" % account
+	if account in state['accounts']:
+		last_tweet = state['accounts'][account]
+	else:
+		last_tweet = 0
+	latest_tweet = 0
+	timeline = api.GetUserTimeline(account, count=200, since_id=latest_tweet)
+	for tweet in timeline:
+		if tweet.id > last_tweet:
+			b.learn(tweet.text)
+			if tweet.id > latest_tweet:
+				latest_tweet = tweet.id
+			tweets += 1
+
+	state['accounts'][account] = latest_tweet
+
+print "Learning %d tweets" % tweets
+b.stop_batch_learning()
+print "Saving data"
+
+open(os.path.join(os.path.dirname(__file__), '.state'), 'w').write(dumps(state))
