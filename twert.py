98d53ad26624da4e86b20ba4acdea88bd8e08971
Dominic <dom@deadempires.net>
2012-06-29 09:43:19 +0100

    Add -t/--tweet option to specify custom tweet

--- twert.py
+++ twert.py
@@ -1,27 +1,32 @@
 import twitter, argparse, sys, os
 import config
+from cobe.brain import Brain
 
 parser = argparse.ArgumentParser(description="Post ebooks tweets to twitter.")
 parser.add_argument('-o', '--stdout', action='store_true', help="Output to stdout instead of posting to twitter.")
+parser.add_argument('-t', '--tweet', help="Tweet arbitrary text instead of using the brain.")
+
 args = parser.parse_args()
 
 api = twitter.Api(**config.api)
 
-from cobe.brain import Brain
-
-b = Brain(os.path.join(os.path.dirname(__file__), 'cobe.brain'))
-
-#truncate to 140 characters, do not cut off words
-def smart_truncate(content, length=140):
-    if len(content) <= length:
-        return content
-    else:
-        return content[:length].rsplit(' ', 1)[0]
-
-# get a reply from brain, encode as UTF-8
-tweet = smart_truncate(b.reply("").encode('utf-8', 'replace'))
-
-if args.stdout:
-	print tweet
+if args.tweet:
+	api.PostUpdate(args.tweet)
 else:
-	status = api.PostUpdate(tweet)
+
+	b = Brain(os.path.join(os.path.dirname(__file__), 'cobe.brain'))
+
+	#truncate to 140 characters, do not cut off words
+	def smart_truncate(content, length=140):
+	    if len(content) <= length:
+	        return content
+	    else:
+	        return content[:length].rsplit(' ', 1)[0]
+
+	# get a reply from brain, encode as UTF-8
+	tweet = smart_truncate(b.reply("").encode('utf-8', 'replace'))
+
+	if args.stdout:
+		print tweet
+	else:
+		status = api.PostUpdate(tweet)
